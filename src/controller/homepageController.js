4f9926a5a7bd0e3952ef7f4d6e5f1b0b4f718d3f
dragonfarm69 <vuminhlongcm123@gmail.com>
2024-01-18 03:37:34 +0700

    update category menu for product_detail and product_list

--- src/controller/homepageController.js
+++ src/controller/homepageController.js
@@ -5,26 +5,21 @@ const {
     dynamic_scroll_pagination,
     calc_total_page,
 } = require("../module/utils");
+const adminModel = require('../model/Admin.m')
+const utils = require('../module/utils')
 
 class HomePageController {
-    async ShowHomePage(req, res) {
-        try {
-            const allProducts = await productModel.getAllProducts();
-            res.render('user/homepage', { 
-            products: allProducts,
-        });
-
-        } catch (error) {
-          console.error("error: ", error);
-        }
-    }
-
     async getAllProduct(req, res, next) {
         const page = isNaN(req.query.page) ? 1 : Number(req.query.page);
         const per_page = isNaN(req.query.per_page)
             ? 12
             : Number(req.query.per_page);
         const max_display_pages = 4;
+
+        let cates = await adminModel.getAll("Category");
+        cates = utils.divideCategories(cates);
+        if (!cates) next(new Error("Error occurred, Please try again"));
+
         try {
             let products = await productModel.get_all();
             if (!products) {
@@ -42,6 +37,7 @@ class HomePageController {
                     total_pages,
                     current_page: page,
                     base_url: "/user/list?",
+                    cates
                 });
             }
         } catch (err) {
@@ -52,11 +48,17 @@ class HomePageController {
     async getByID(req, res, next){ 
         try {
             let product = await productModel.get(req.params.product_id);
+
+            let cates = await adminModel.getAll("Category");
+            cates = utils.divideCategories(cates);
+            if (!cates) next(new Error("Error occurred, Please try again"));
+
             if (!product) {
                 next(new Error("Error occurred, Please try again"));
             } else {
                 res.render("user/product_detail", {
-                    product
+                    product,
+                    cates
                 });
             }
         } catch (err) {
@@ -73,6 +75,11 @@ class HomePageController {
         try {
             let type = req.query.type;
             let products = await productModel.getByCategory(type);
+
+            let cates = await adminModel.getAll("Category");
+            cates = utils.divideCategories(cates);
+            if (!cates) next(new Error("Error occurred, Please try again"));
+
             if (!products) {
                 next(new Error("Error occurred, Please try again"));
             } else {
@@ -80,7 +87,7 @@ class HomePageController {
                     max_display_pages,
                     per_page,
                     page,
-                    products
+                    products,
                 );
                 res.render("user/product_list", {
                     products: items,
@@ -88,6 +95,7 @@ class HomePageController {
                     total_pages,
                     current_page: page,
                     base_url: "/user/list?",
+                    cates
                 });
             }
         } catch (err) {
@@ -104,8 +112,14 @@ class HomePageController {
         try {
             let pattern = req.query.pattern
             let products = await productModel.getByPattern(pattern);
+
+            let cates = await adminModel.getAll("Category");
+            cates = utils.divideCategories(cates);
+            if (!cates) next(new Error("Error occurred, Please try again"));
+
             if (!products) {
-                next(new Error("Error occurred, Please try again"));
+            next(new Error("Error occurred, Please try again"));
+            
             } else {
                 const { pages, items, total_pages } = dynamic_scroll_pagination(
                     max_display_pages,
@@ -119,6 +133,7 @@ class HomePageController {
                     total_pages,
                     current_page: page,
                     base_url: "/user/list?",
+                    cates
                 });
             }
         } catch (err) {
--- src/route/userRoute.js
+++ src/route/userRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const homepage = require('../controller/homepageController');
-const pagination_util = require('../module/utils')
 const database = require('../model/Product.model');
 const homepageController = require('../controller/homepageController');
 const adminModel = require('../model/Admin.m')
@@ -13,11 +12,14 @@ router.get('/', async (req, res) => {
         array.fill({role: "admin", text: "hello motherf jeqwoie jioqwje oucker"});
         array = array.concat(new Array(10).fill({role: "customer", text: "hello motherf jeqwoie jioqwje oucker"}))
                     .sort(() => (Math.random() > .5) ? 1 : -1);
+
         let allProducts = await database.get_all();
         let newProducts = allProducts.slice(0, 4);
+
         let cates = await adminModel.getAll("Category");
         cates = utils.divideCategories(cates);
         if (!cates) next(new Error("Error occurred, Please try again"));
+
         else {
             cates = cates.map((cate) => {
                 return {
@@ -39,10 +41,15 @@ router.get('/detail', async(req, res) => {
     const productId = req.query.product_id;
     let product = await database.get(productId);
     let relatedProduct = await database.getRelatedProducts(product);
+
+    let cates = await adminModel.getAll("Category");
+    cates = utils.divideCategories(cates);
+    if (!cates) next(new Error("Error occurred, Please try again"));
   
     res.render("user/product_detail", { 
         product,
-        relatedProduct
+        relatedProduct,
+        cates
     });
 });
 
